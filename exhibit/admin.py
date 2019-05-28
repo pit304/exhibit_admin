6a3a31bbebc970a110cf9d16df8cbd7589431d5e
Alexandru Petrencu <alex.petrencu@gmail.com>
2019-05-28 08:31:04 +0200

    updated admin site

--- exhibit/admin.py
+++ exhibit/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 
-from .models import Atelier, Project, Publication, Competition
+from .models import Atelier, Project, Publication, Competition, Image, Plan
 
+admin.site.site_header = 'Exhibit administration'
+admin.site.site_title = 'Exhibit site admin'
+admin.site.index_title = 'Site administration'
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 3
+
+class PlanInline(admin.TabularInline):
+    model = Plan
+    extra = 3
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['project_title', 'project_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ImageInline, PlanInline]
+    list_display = ('project_title', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['project_title']
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Atelier)
-admin.site.register(Project)
 admin.site.register(Publication)
 admin.site.register(Competition)
--- exhibit/models.py
+++ exhibit/models.py
@@ -12,6 +12,7 @@ class Project(models.Model):
     project_title = models.CharField(max_length=200)
     project_text = models.TextField(max_length=2000)
     pub_date = models.DateTimeField('date published')
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.project_title
@@ -20,8 +21,23 @@ class Project(models.Model):
         now = timezone.now()
         return now - timedelta(days=1) <= self.pub_date <= now
 
-class Publication(models.Model):
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    
+class Image(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    image_title = models.CharField(max_length=200)
+    image_url = models.URLField()
+    active = models.BooleanField(default=True)
+
+class Plan(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    plan_title = models.CharField(max_length=200)
+    plan_url = models.URLField()
+    active = models.BooleanField(default=True)
+
+class Publication(models.Model):
     publication_title = models.CharField(max_length=200)
     publication_text = models.TextField(max_length=2000)
     year = models.IntegerField(default=0)
